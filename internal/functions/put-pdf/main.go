c2fcb7847dcd512043d5e70fdddfb2d2fe9d9305
Joel Teodoro <joel.teodoro@dribba.com>
2026-08-06 19:15:10 +0200

    feature(dynamo): adding dynamo db storage

---
+++ internal/functions/put-pdf/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	tableName, ok := os.LookupEnv("TABLE")
+	if !ok {
+		panic("Need TABLE environment variable")
+	}
+
+	dynamodb := store.NewDynamoDBStore(context.TODO(), tableName)
+	domain := domain.NewProductsDomain(dynamodb)
+	handler := handlers.NewAPIGatewayV2Handler(domain)
+	lambda.Start(handler.DeleteHandler)
+}
